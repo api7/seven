004bdb882bfdea675f8032e85e1c099d5d36e283
kv <gxthrj@163.com>
2020-02-13 15:10:24 +0800

    fix make upstreams in diff crds in order

--- state/solver.go
+++ state/solver.go
@@ -1,5 +1,20 @@
 package state
 
+import "github.com/gxthrj/apisix-types/pkg/apis/apisix/v1"
+
+var UpstreamQueue chan UpstreamQueueObj
+func init(){
+	UpstreamQueue =  make(chan UpstreamQueueObj, 500)
+	go WatchUpstream()
+}
+
+func WatchUpstream(){
+	for{
+		uqo := <- UpstreamQueue
+		SolverUpstream(uqo.Upstreams, uqo.ServiceWorkerGroup)
+	}
+}
+
 // Solver
 func (s *ApisixCombination)Solver() (bool, error){
 	// 1.route workers
@@ -7,6 +22,19 @@ func (s *ApisixCombination)Solver() (bool, error){
 	// 2.service workers
 	swg := NewServiceWorkers(s.Services, &rwg)
 	// 3.upstream workers
-	SolverUpstream(s.Upstreams, swg)
+	//SolverUpstream(s.Upstreams, swg)
+	uqo := &UpstreamQueueObj{Upstreams: s.Upstreams, ServiceWorkerGroup: swg}
+	uqo.AddQueue()
 	return true, nil
+}
+// UpstreamQueueObj for upstream queue
+type UpstreamQueueObj struct {
+	Upstreams []*v1.Upstream
+	ServiceWorkerGroup ServiceWorkerGroup
+}
+
+// AddQueue make upstreams in order
+// upstreams is group by CRD
+func (uqo *UpstreamQueueObj) AddQueue(){
+	UpstreamQueue <- *uqo
 }
