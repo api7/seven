ec56e0eabd8f6362721fb6797c70268f1e8e70b6
kv <gxthrj@163.com>
2020-02-14 17:49:09 +0800

    modify ApisixService solver in order

--- state/service_worker.go
+++ state/service_worker.go
@@ -2,14 +2,18 @@ package state
 
 import (
 	"github.com/gxthrj/apisix-types/pkg/apis/apisix/v1"
+	"github.com/golang/glog"
+	"strings"
 	"github.com/gxthrj/seven/DB"
 	"github.com/gxthrj/seven/apisix"
-	"github.com/gxthrj/seven/conf"
-	"github.com/golang/glog"
 	"github.com/gxthrj/seven/utils"
 	"strconv"
-	"strings"
+	"github.com/gxthrj/seven/conf"
 )
+
+
+const ApisixService = "ApisixService"
+
 
 type serviceWorker struct {
 	*v1.Service
@@ -34,58 +38,124 @@ func (w *serviceWorker) start(rwg *RouteWorkerGroup) {
 		}
 	}()
 }
-
+// trigger add to queue
 func (w *serviceWorker) trigger(event Event, rwg *RouteWorkerGroup) error {
 	glog.Infof("1.service trigger from %s, %s", event.Op, event.Kind)
 	defer close(w.Quit)
 	// consumer Event set upstreamID
 	upstream := event.Obj.(*v1.Upstream)
 	glog.Infof("2.service trigger from %s, %s", event.Op, *upstream.Name)
-	w.UpstreamId = upstream.ID
 
-	op := Update
-	// padding
-	currentRoute, _ := apisix.FindCurrentService(*w.Service.Name)
-	paddingService(w.Service, currentRoute)
-	// diff
-	hasDiff, err := utils.HasDiff(w.Service, currentRoute)
-	// sync
-	if err != nil {
-		return err
-	}
-	if hasDiff {
-		if *w.Service.ID == strconv.Itoa(0) {
-			op = Create
-			// 1. sync apisix and get id
-			if serviceResponse, err := apisix.AddService(w.Service, conf.BaseUrl); err != nil {
-				// todo log error
-				glog.Info(err.Error())
-			}else {
-				tmp := strings.Split(*serviceResponse.Service.Key, "/")
-				*w.Service.ID = tmp[len(tmp) - 1]
-			}
-			// 2. sync memDB
-			db := &DB.ServiceDB{Services: []*v1.Service{w.Service}}
-			db.Insert()
-			glog.Infof("create service %s, %s", *w.Name, *w.UpstreamId)
-		}else {
-			op = Update
-			// 1. sync memDB
-			db := DB.ServiceDB{Services: []*v1.Service{w.Service}}
-			if err := db.UpdateService(); err != nil {
-				// todo log error
-			}
-			// 2. sync apisix
-			apisix.UpdateService(w.Service, conf.BaseUrl)
-			glog.Infof("update service %s, %s", *w.Name, *w.UpstreamId)
+	w.UpstreamId = upstream.ID
+	// add to queue
+	services := []*v1.Service{w.Service}
+	sqo := &ServiceQueueObj{Services: services, RouteWorkerGroup: *rwg}
+	sqo.AddQueue()
+
+	//op := Update
+	//// padding
+	//currentService, _ := apisix.FindCurrentService(*w.Service.Name)
+	//paddingService(w.Service, currentService)
+	//// diff
+	//hasDiff, err := utils.HasDiff(w.Service, currentService)
+	//// sync
+	//if err != nil {
+	//	return err
+	//}
+	//if hasDiff {
+	//	if *w.Service.ID == strconv.Itoa(0) {
+	//		op = Create
+	//		// 1. sync apisix and get id
+	//		if serviceResponse, err := apisix.AddService(w.Service, conf.BaseUrl); err != nil {
+	//			// todo log error
+	//			glog.Info(err.Error())
+	//		}else {
+	//			tmp := strings.Split(*serviceResponse.Service.Key, "/")
+	//			*w.Service.ID = tmp[len(tmp) - 1]
+	//		}
+	//		// 2. sync memDB
+	//		db := &DB.ServiceDB{Services: []*v1.Service{w.Service}}
+	//		db.Insert()
+	//		glog.Infof("create service %s, %s", *w.Name, *w.UpstreamId)
+	//	}else {
+	//		op = Update
+	//		// 1. sync memDB
+	//		db := DB.ServiceDB{Services: []*v1.Service{w.Service}}
+	//		if err := db.UpdateService(); err != nil {
+	//			// todo log error
+	//		}
+	//		// 2. sync apisix
+	//		apisix.UpdateService(w.Service, conf.BaseUrl)
+	//		glog.Infof("update service %s, %s", *w.Name, *w.UpstreamId)
+	//	}
+	//}
+	//// broadcast to route
+	//routeWorkers := (*rwg)[*w.Service.Name]
+	//for _, rw := range routeWorkers{
+	//	event := &Event{Kind: ServiceKind, Op: op, Obj: w.Service}
+	//	glog.Infof("send event %s, %s, %s", event.Kind, event.Op, *w.Service.Name)
+	//	rw.Event <- *event
+	//}
+	return nil
+}
+
+func SolverService(services []*v1.Service, rwg RouteWorkerGroup) error{
+	for _, svc := range services {
+		op := Update
+		// padding
+		currentService, _ := apisix.FindCurrentService(*svc.Name)
+		paddingService(svc, currentService)
+		// diff
+		hasDiff, err := utils.HasDiff(svc, currentService)
+		// sync
+		if err != nil {
+			return err
 		}
-	}
-	// broadcast to route
-	routeWorkers := (*rwg)[*w.Service.Name]
-	for _, rw := range routeWorkers{
-		event := &Event{Kind: ServiceKind, Op: op, Obj: w.Service}
-		glog.Infof("send event %s, %s, %s", event.Kind, event.Op, *w.Service.Name)
-		rw.Event <- *event
+		if hasDiff {
+			if *svc.ID == strconv.Itoa(0) {
+				op = Create
+				// 1. sync apisix and get id
+				if serviceResponse, err := apisix.AddService(svc, conf.BaseUrl); err != nil {
+					// todo log error
+					glog.Info(err.Error())
+				}else {
+					tmp := strings.Split(*serviceResponse.Service.Key, "/")
+					*svc.ID = tmp[len(tmp) - 1]
+				}
+				// 2. sync memDB
+				db := &DB.ServiceDB{Services: []*v1.Service{svc}}
+				db.Insert()
+				glog.Infof("create service %s, %s", *svc.Name, *svc.UpstreamId)
+			}else {
+				op = Update
+				needToUpdate := true
+				if currentService.FromKind != nil && *(currentService.FromKind) == ApisixService { // update from ApisixUpstream
+					if svc.FromKind == nil || (svc.FromKind != nil && *(svc.FromKind) != ApisixService) {
+						// currentService > svc
+						// set lb && health check
+						needToUpdate = false
+					}
+				}
+				if needToUpdate{
+					// 1. sync memDB
+					db := DB.ServiceDB{Services: []*v1.Service{svc}}
+					if err := db.UpdateService(); err != nil {
+						// todo log error
+					}
+					// 2. sync apisix
+					apisix.UpdateService(svc, conf.BaseUrl)
+					glog.Infof("update service %s, %s", *svc.Name, *svc.UpstreamId)
+				}
+
+			}
+		}
+		// broadcast to route
+		routeWorkers := rwg[*svc.Name]
+		for _, rw := range routeWorkers{
+			event := &Event{Kind: ServiceKind, Op: op, Obj: svc}
+			glog.Infof("send event %s, %s, %s", event.Kind, event.Op, *svc.Name)
+			rw.Event <- *event
+		}
 	}
 	return nil
 }
--- state/solver.go
+++ state/solver.go
@@ -3,9 +3,21 @@ package state
 import "github.com/gxthrj/apisix-types/pkg/apis/apisix/v1"
 
 var UpstreamQueue chan UpstreamQueueObj
+var ServiceQueue chan ServiceQueueObj
+
 func init(){
 	UpstreamQueue =  make(chan UpstreamQueueObj, 500)
+	ServiceQueue =  make(chan ServiceQueueObj, 500)
 	go WatchUpstream()
+	go WatchService()
+}
+
+func WatchService(){
+	for{
+		sqo := <- ServiceQueue
+		// solver service
+		SolverService(sqo.Services, sqo.RouteWorkerGroup)
+	}
 }
 
 func WatchUpstream(){
@@ -21,6 +33,8 @@ func (s *ApisixCombination)Solver() (bool, error){
 	rwg := NewRouteWorkers(s.Routes)
 	// 2.service workers
 	swg := NewServiceWorkers(s.Services, &rwg)
+	//sqo := &ServiceQueueObj{Services: s.Services, RouteWorkerGroup: rwg}
+	//sqo.AddQueue()
 	// 3.upstream workers
 	uqo := &UpstreamQueueObj{Upstreams: s.Upstreams, ServiceWorkerGroup: swg}
 	uqo.AddQueue()
@@ -36,4 +50,15 @@ type UpstreamQueueObj struct {
 // upstreams is group by CRD
 func (uqo *UpstreamQueueObj) AddQueue(){
 	UpstreamQueue <- *uqo
+}
+
+type ServiceQueueObj struct {
+	Services []*v1.Service
+	RouteWorkerGroup RouteWorkerGroup
+}
+
+// AddQueue make upstreams in order
+// upstreams is group by CRD
+func (sqo *ServiceQueueObj) AddQueue(){
+	ServiceQueue <- *sqo
 }
