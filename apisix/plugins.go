e61241a5f4efae3d3c0604e435a635f2e87021e0
kv <gxthrj@163.com>
2020-02-14 14:16:42 +0800

    add cors plugin

--- apisix/plugins.go
+++ apisix/plugins.go
@@ -9,7 +9,7 @@ type IpRestriction struct {
 }
 
 // Convert2IpRestriction build IpRestriction
-func BuildIpRestriction(whites, blacks *string) *IpRestriction{
+func BuildIpRestriction(whites, blacks *string) *IpRestriction {
 	result := &IpRestriction{}
 	if whites != nil {
 		whiteIps := strings.Split(*whites, ",")
@@ -20,4 +20,36 @@ func BuildIpRestriction(whites, blacks *string) *IpRestriction{
 		result.Blacklist = blackIps
 	}
 	return result
+}
+
+type Cors struct {
+	Origins []string `json:"origins,omitempty"`
+	Headers []string `json:"headers,omitempty"`
+	Methods []string `json:"methods,omitempty"`
+	MaxAge  int64    `json:"max_age,omitempty"`
+}
+
+// BuildCors
+func BuildCors(enable bool, originStr, headerStr, methodStr *string, maxAge *int64) *Cors{
+	result := &Cors{}
+	if enable {
+		if originStr != nil {
+			origins := strings.Split(*originStr, ",")
+			result.Origins = origins
+		}
+		if headerStr != nil {
+			headers := strings.Split(*headerStr, ",")
+			result.Headers = headers
+		}
+		if methodStr != nil {
+			methods := strings.Split(*methodStr, ",")
+			result.Methods = methods
+		}
+		if maxAge != nil {
+			result.MaxAge = *maxAge
+		}
+		return result
+	}else {
+		return nil
+	}
 }
