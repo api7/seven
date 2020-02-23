796d945a13d020f8478dca843e06153bd53ba331
kv <gxthrj@163.com>
2020-02-23 13:19:01 +0800

    add BaseUrl conf func

--- conf/conf.go
+++ conf/conf.go
@@ -3,6 +3,10 @@ package conf
 var BaseUrl = "http://172.16.20.90:30116/apisix/admin"
 var UrlGroup = make(map[string]string)
 
+func SetBaseUrl(url string){
+	BaseUrl = url
+}
+
 func AddGroup(group string){
 	if group != "" {
 		UrlGroup[group] = "http://" + group + "/apisix/admin"
